3229730edaec75621a274d2fd8bd8b7644803f76
Anish <srtanish1992@gmail.com>
2019-11-10 10:09:26 -0800

    input string to array

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import TweetContainer from './tweetContainer';
 
 class Input extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            tweets: [],
+            inputString: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({inputString:e.target.value});
+    }
+
+    splitInputString = () => {
+        return this.state.inputString.replace(/\s/g, '').split(",");
+    }
+
+    getTweets = () => {
+        let inputStringArray = this.splitInputString();
+        console.log(inputStringArray);
+    }
+
     render(){
         return(
 
@@ -14,15 +37,21 @@ class Input extends React.Component {
                         label="Outlined"
                         margin="normal"
                         variant="outlined"
+                        onChange={this.handleChange}
                     />
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <Button 
                         variant="outlined" 
                         // className={classes.button}
+                        onClick ={this.getTweets}
                         >
                         Search
                     </Button>
                 </div>
+
+                {this.state.tweets > 0 && 
+                    <TweetContainer tweets = {this.state.tweets}/>
+                }
             </React.Fragment>
 
 
